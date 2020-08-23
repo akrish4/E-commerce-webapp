3dc073891ae242fa8dc1085b3f0a050a2cd81720
ananthakrishnan <ananthakrishnan20121999@gmail.com>
2020-08-23 10:02:13 +0530

    updated

--- includes/common.php
+++ includes/common.php
@@ -1,7 +1,6 @@
 <?php
-    $con = mysqli_connect("localhost", "admin", "admin4321", "store","8000")
-    or die(mysqli_error($con));
-    if(!isset($_SESSION)){
-      session_start();
-    }
-?>
+$con = mysqli_connect("localhost", "admin", "admin4321", "store")
+  or die(mysqli_error($con));
+if (!isset($_SESSION)) {
+  session_start();
+}
